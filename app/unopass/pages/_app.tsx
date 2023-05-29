c910b4cd59025e52c42591f67079c664be5462da
Nabil Ridhwan <nabridhwan@gmail.com>
2023-05-29 22:43:51 +0800

    feat: :sparkles: add OnboardProvider to provide for app

--- app/unopass/pages/_app.tsx
+++ app/unopass/pages/_app.tsx
@@ -1,13 +1,48 @@
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { useIfUserNeedsOnboard } from "@/query/user.query";
+import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
+
+const queryClient = new QueryClient();
+
+function OnboardProvider({ children }: { children: ReactNode }) {
+	const { user } = useUser();
+	const router = useRouter();
+
+	const { mutateAsync, error, data } = useIfUserNeedsOnboard(
+		user?.email || ""
+	);
+
+	useEffect(() => {
+		if (!user) return;
+
+		if (user && !error && user.email) {
+			mutateAsync({
+				email: user.email,
+			});
+		}
+	}, [user]);
+
+	if (data && data.error) {
+		router.replace("/onboard");
+	}
+
+	return <>{children}</>;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<UserProvider>
-			<ChakraProvider>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</UserProvider>
+		<QueryClientProvider client={queryClient}>
+			<UserProvider>
+				<OnboardProvider>
+					<ChakraProvider>
+						<Component {...pageProps} />
+					</ChakraProvider>
+				</OnboardProvider>
+			</UserProvider>
+		</QueryClientProvider>
 	);
 }
