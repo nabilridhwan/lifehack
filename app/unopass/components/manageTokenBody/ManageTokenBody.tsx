fcddd8efb1bada7ed8cdd67def345a2bb266ea34
shayaanak32 <destinedfrsuccess09@gmail.com>
2023-05-30 02:34:58 +0800

    refactor: :recycle: allowed for props to be passed till TokenCards
    the database isn't retrieving the actual data, but props are correctly defined and not undefined

--- app/unopass/components/manageTokenBody/ManageTokenBody.tsx
+++ app/unopass/components/manageTokenBody/ManageTokenBody.tsx
@@ -4,6 +4,7 @@ import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
 import {BiBarcode} from 'react-icons/bi';
 import { TfiPlus,TfiSupport } from "react-icons/tfi";
 import TokenCards from "../tokenCards/TokenCards";
+import {TravelData} from "../../pages/manage_tokens/index"
 interface FeatureProps {
     title: string;
     text: string;
@@ -16,40 +17,47 @@ const Feature = ({ title, text, icon }: FeatureProps) => {
             <Flex
                 w={16}
                 h={16}
-                align={'center'}
-                justify={'center'}
-                color={'white'}
-                rounded={'full'}
-                bg={'gray.100'}
-                mb={1}>
+                align={"center"}
+                justify={"center"}
+                color={"white"}
+                rounded={"full"}
+                bg={"gray.100"}
+                mb={1}
+            >
                 {icon}
             </Flex>
             <Text fontWeight={600}>{title}</Text>
-            <Text color={'gray.600'}>{text}</Text>
+            <Text color={"gray.600"}>{text}</Text>
         </Stack>
     );
 };
 const myPropObject = {
-    name: 'Istanbul Trip Token',
-    generated: new Date('2022-10-11'),
-  };
-  const myPropObject2 = {
-    name: 'Bali Trip Token',
-    generated: new Date('2022-10-11'),
-  };
+    name: "Istanbul Trip Token",
+    generated: new Date("2022-10-11"),
+};
+const myPropObject2 = {
+    name: "Bali Trip Token",
+    generated: new Date("2022-10-11"),
+};
 
-  const myPropObject3 = {
-    name: 'Karachi Trip Token',
-    generated: new Date('2022-10-11'),
-  };
+const myPropObject3 = {
+    name: "Karachi Trip Token",
+    generated: new Date("2022-10-11"),
+};
+interface ManageTokenBodyProps {
+    totalData: TravelData; // Assuming TravelData is the correct type for the totalData prop
+}
 
-export default function ManageTokenBody() {
+export default function ManageTokenBody({ totalData }: ManageTokenBodyProps) {
     return (
         <Box p={14}>
             <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} padding={25}>
-                <TokenCards myPropObject = {myPropObject}/>
-                <TokenCards myPropObject = {myPropObject2}/>
-                <TokenCards myPropObject = {myPropObject3}/>
+                <TokenCards myPropObject={totalData} />
+                <TokenCards myPropObject={totalData} />
+                <TokenCards myPropObject={totalData} />
+
+                {/* <TokenCards myPropObject={myPropObject2} />
+                <TokenCards myPropObject={myPropObject3} /> */}
                 {/* <Feature
                     icon={<Icon as={TfiPlus} w={10} h={10} />}
                     title={'Create Tokens'}
--- app/unopass/components/tokenCards/TokenCards.tsx
+++ app/unopass/components/tokenCards/TokenCards.tsx
@@ -1,80 +1,114 @@
 import {
     Box,
+    Button,
     Center,
-    useColorModeValue,
     Heading,
-    Text,
     Stack,
-    Image,
-    chakra
-} from '@chakra-ui/react';
+    Text,
+    chakra,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import {TravelData} from "../../pages/manage_tokens/index"
 
 const IMAGE =
-    'https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80';
+    "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80";
 
-const TokenCards = chakra((props) => {
-    const { name, generated } = props;
+interface TokenCardsProps {
+    // flight_number: string;
+    // seat_number: string;
+    // gate_number: string;
+    // terminal: string;
+    // airline: string;
+    // row_number: string
+    myPropObject: TravelData;
+
+}
+const TokenCards: React.FC<TokenCardsProps> = ({myPropObject}) => {
+    console.log(myPropObject);
     return (
         <Center py={12}>
             <Box
-                role={'group'}
+                role={"group"}
                 p={6}
-                maxW={'330px'}
-                w={'full'}
-                bg={useColorModeValue('white', 'gray.800')}
-                boxShadow={'2xl'}
-                rounded={'lg'}
-                pos={'relative'}
-                zIndex={1}>
-                <Box
-                    rounded={'lg'}
-                    mt={-12}
-                    pos={'relative'}
-                    height={'230px'}
-                    _after={{
-                        transition: 'all .3s ease',
-                        content: '""',
-                        w: 'full',
-                        h: 'full',
-                        pos: 'absolute',
-                        top: 5,
-                        left: 0,
-                        backgroundImage: `url(${IMAGE})`,
-                        filter: 'blur(15px)',
-                        zIndex: -1,
-                    }}
-                    _groupHover={{
-                        _after: {
-                            filter: 'blur(20px)',
-                        },
-                    }}>
-                    <Image
-                        rounded={'lg'}
-                        height={230}
-                        width={282}
-                        objectFit={'cover'}
-                        marginTop={10}
-                        src={IMAGE}
-                    />
-                </Box>
-                <Stack pt={10} align={'center'}>
-                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                        Brand
+                maxW={"330px"}
+                w={"full"}
+                bg={useColorModeValue("white", "gray.800")}
+                boxShadow={"2xl"}
+                rounded={"lg"}
+                pos={"relative"}
+                zIndex={1}
+            >
+                {/* <Box
+					rounded={"lg"}
+					mt={-12}
+					pos={"relative"}
+					height={"230px"}
+					// _after={{
+					//     transition: 'all .3s ease',
+					//     content: '""',
+					//     w: 'full',
+					//     h: 'full',
+					//     pos: 'absolute',
+					//     top: 5,
+					//     left: 0,
+					//     backgroundImage: `url(${IMAGE})`,
+					//     filter: 'blur(15px)',
+					//     zIndex: -1,
+					// }}
+					// _groupHover={{
+					//     _after: {
+					//         filter: 'blur(20px)',
+					//     },
+					// }}
+				>
+					<Image
+						rounded={"lg"}
+						height={230}
+						width={282}
+						objectFit={"cover"}
+						marginTop={10}
+						src={IMAGE}
+					/>
+				</Box> */}
+                <Stack pt={10} align={"center"}>
+                    <Text
+                        color={"gray.500"}
+                        fontSize={"sm"}
+                        textTransform={"uppercase"}
+                    >
+                        {/* {myPropObject.airline} */}
+                        Singapore Airlines
                     </Text>
-                    <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                        $57
+                    <Heading
+                        fontSize={"2xl"}
+                        fontFamily={"body"}
+                        fontWeight={500}
+                    >
+                        SQ2490 
                     </Heading>
-                    <Stack direction={'row'} align={'center'}>
-                        <Text fontWeight={800} fontSize={'xl'}>
-                            $199
-                        </Text>
+                    <Text
+                        color={"gray.500"}
+                        fontSize={"sm"}
+                    >
+                        {/* {myPropObject.airline} */}
+                        Departure: 20/05/22 11pm SGT
+                    </Text>
+                    <Text
+                        color={"gray.500"}
+                        fontSize={"sm"}
+                    >
+                        {/* {myPropObject.airline} */}
+                        Arrival: 21/05/22 2pm SGT
+                    </Text>
+                    <Stack direction={"row"} align={"center"}>
+
+                        <Button>Generate QR Code</Button>
                     </Stack>
                 </Stack>
             </Box>
         </Center>
     );
-
-});
+};
 
 export default TokenCards;
 //   export default function TokenCards() {
