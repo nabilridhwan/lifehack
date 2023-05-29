d17f9ac93ddfbea9f2a7e66e16b5a6e6d14f5b0b
Nabil Ridhwan <nabridhwan@gmail.com>
2023-05-30 00:27:23 +0800

    feat: ui changes

--- app/unopass/components/nav/Nav.tsx
+++ app/unopass/components/nav/Nav.tsx
@@ -1,69 +1,60 @@
-import React from "react";
-import {
-    Box,
-    Flex,
-    HStack,
-    Heading,
-    Icon,
-    Link,
-    Spacer,
-} from "@chakra-ui/react";
+import { Box, Flex, HStack, Heading, Link, Spacer } from "@chakra-ui/react";
 
 export const links = [
-    {
-        id: 1,
-        link: "/",
-        name: "Home",
-    },
-    { id: 2, link: "/manage_tokens", name: "Manage Tokens" },
-    {
-        id: 3,
-        link: "/create_tokens",
-        name: "Create Tokens",
-    },
-    {
-        id: 4,
-        link: "/scan_tokens",
-        name: "Scan Tokens",
-    },
- ];
- 
- const Nav = () => {
-    return (
-        <Flex
-            w='full'
-            alignItems='center'
-            minHeight={"10vh"}
-            padding={{ base: "30px", md: "30px 70px" }}
-            justifyContent='space-between'
-        >
-            <Box>
-                <Heading as='h2' size='md' color='#808080'>
-                    UNOPASS
-                </Heading>
-            </Box>
+	{
+		id: 1,
+		link: "/",
+		name: "Home",
+	},
+	{ id: 2, link: "/manage_tokens", name: "Manage Tokens" },
+	{
+		id: 3,
+		link: "/create_tokens",
+		name: "Create Tokens",
+	},
+	{
+		id: 4,
+		link: "/scan_tokens",
+		name: "Scan Tokens",
+	},
+];
 
-            <Spacer />
+const Nav = () => {
+	return (
+		<Flex
+			w="full"
+			alignItems="center"
+			minHeight={"10vh"}
+			padding={{ base: "30px", md: "30px 70px" }}
+			justifyContent="space-between"
+		>
+			<Box>
+				<Heading as="h2" size="md">
+					UNOPASS
+				</Heading>
+			</Box>
 
-            <HStack spacing='30px' display={{ base: "none", lg: "inline-block" }}>
-                {links.map((li) => (
-                    <Link
-                        href={li.link}
-                        color='#000000'
-                        opacity={0.7}
-                        fontWeight='semibold'
-                        key={li.id}
-                    >
-                        {li.name}
-                    </Link>
-                ))}
-            </HStack>
+			<Spacer />
 
-            <Spacer />
+			<HStack
+				spacing="30px"
+				display={{ base: "none", lg: "inline-block" }}
+			>
+				{links.map((li) => (
+					<Link
+						href={li.link}
+						opacity={0.7}
+						fontWeight="semibold"
+						key={li.id}
+					>
+						{li.name}
+					</Link>
+				))}
+			</HStack>
 
-            
-        </Flex>
-    );
+			<Spacer />
+		</Flex>
+	);
 };
 
 export default Nav;
--- app/unopass/pages/token_created_page/index.tsx
+++ app/unopass/pages/token_created_page/index.tsx
@@ -1,17 +1,60 @@
-
-
-import { Heading } from '@chakra-ui/react'
-import { Container, HStack, VStack } from '@chakra-ui/react'
+import { Box, Icon, Stack, Text } from "@chakra-ui/react";
+import { useQRCode } from "next-qrcode";
+import { BiPaperPlane } from "react-icons/bi";
 
 export default function TokenCreatedPage() {
-    return (
-        <>
-            <Heading ml={130} mt={50} mb={50}>Token Has Been Generated!!!!</Heading>
-            <Container maxW='2x1' bg='grey' color='black'>
-                QR CODE HERE
-            </Container>
+	const { Canvas } = useQRCode();
 
-        </>
-    );
+	return (
+		<>
+			<Stack alignItems={"center"}>
+				<Box borderRadius={20} bg="white" m={20}>
+					<Canvas
+						text={"https://github.com/bunlong/next-qrcode"}
+						options={{
+							level: "M",
+							margin: 3,
+							scale: 4,
+							width: 200,
+							color: {
+								dark: "#000",
+								light: "#FFF",
+							},
+						}}
+					/>
+				</Box>
+				<Box borderRadius={5} bg="whiteAlpha.100" p={5}>
+					<Stack>
+						<Stack
+							direction={"row"}
+							alignItems={"center"}
+							textColor={"gray.400"}
+						>
+							<Icon
+								w={8}
+								h={8}
+								alignItems={"center"}
+								justifyContent={"center"}
+							>
+								<BiPaperPlane />
+							</Icon>
 
+							<Text>Airline</Text>
+						</Stack>
+						<Text>
+							This QR Code can be used by the user to redeem their
+							token
+						</Text>
+					</Stack>
+				</Box>
+
+				<Box borderRadius={5} bg="whiteAlpha.100" p={5}>
+					<Text>
+						This QR Code can be used by the user to redeem their
+						token
+					</Text>
+				</Box>
+			</Stack>
+		</>
+	);
 }
